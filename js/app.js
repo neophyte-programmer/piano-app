1d99d83e09adedfac211c6fed9e817f7fd0ac027
Nutifafa Attor <attorfafa@gmail.com>
2022-04-12 18:04:02 +0000

    linked sounds to variables

---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Variables for audio
+
+// C Notes
+const cThree = new Audio()
+cThree.src = 'notes/C3.mp3'
+
+const cFour = new Audio()
+cFour.src = 'notes/C4.mp3'
+
+const cFive = new Audio()
+cFive.src = 'notes/C5.mp3'
+
+const cSix = new Audio()
+cSix.src = 'notes/C6.mp3'
+
+// C Sharp Notes
+const cSharpFour = new Audio()
+cSharpFour.src = 'notes/C#4.mp3'
+
+const cSharpFive = new Audio()
+cSharpFive.src = 'notes/C#5.mp3'
+
+// D Notes
+const dThree = new Audio()
+dThree.src = 'notes/D3.mp3'
+
+const dFour = new Audio()
+dFour.src = 'notes/D4.mp3'
+
+const dFive = new Audio()
+dFive.src = 'notes/D5.mp3'
+
+// D Sharp Notes
+const dSharpThree = new Audio()
+dSharpThree.src = 'notes/D#3.mp3'
+
+const dSharpFour = new Audio()
+dSharpFour.src = 'notes/D#4.mp3'
+
+// E Notes
+const eThree = new Audio()
+eThree.src = 'notes/E3.mp3'
+
+const eFour = new Audio()
+eFour.src = 'notes/E4.mp3'
+
+const eFive = new Audio()
+eFive.src = 'notes/E5.mp3'
+
+// F Notes
+const fThree = new Audio()
+fThree.src = 'notes/F3.mp3'
+
+const fFour = new Audio()
+fFour.src = 'notes/F4.mp3'
+
+// F Sharp Notes
+const fSharpThree = new Audio()
+fSharpThree.src = 'notes/F#3.mp3'
+
+const fSharpFour = new Audio()
+fSharpFour.src = 'notes/F#4.mp3'
+
+const fSharpFive = new Audio()
+fSharpFive.src = 'notes/F#5.mp3'
+
+// G Notes
+const gThree = new Audio()
+gThree.src = 'notes/G3.mp3'
+
+const gFour = new Audio()
+gFour.src = 'notes/G4.mp3'
+
+const gFive = new Audio()
+gFive.src = 'notes/G5.mp3'
+
+// G Sharp Notes
+const gSharpFour = new Audio()
+gSharpFour.src = 'notes/G#4.mp3'
+
+// A Notes
+const aThree = new Audio()
+aThree.src = 'notes/A3.mp3'
+
+const aFour = new Audio()
+aFour.src = 'notes/A4.mp3'
+
+const aFive = new Audio()
+aFive.src = 'notes/A5.mp3'
+
+// A Sharp Notes
+const aSharpThree = new Audio()
+aSharpThree.src = 'notes/A#3.mp3'
+
+const aSharpFour = new Audio()
+aSharpFour.src = 'notes/A#4.mp3'
+
+const aSharpFive = new Audio()
+aSharpFive.src = 'notes/A#5.mp3'
+
+// B Notes
+const bThree = new Audio()
+bThree.src = 'notes/B3.mp3'
+
+const bFour = new Audio()
+bFour.src = 'notes/B4.mp3'
+
+const bFive = new Audio()
+bFive.src = 'notes/B5.mp3'
